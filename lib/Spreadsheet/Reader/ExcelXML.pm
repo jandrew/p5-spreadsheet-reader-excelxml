529ba7e03c8dbb947fadb8b71e336e4a39134e86
Jed Lund <jandrew@cpan.org>
2016-05-05 11:38:06 -0700

    v0.1_25 ! Another travis-ci fix attempt
    Signed-off-by: Jed Lund <jandrew@cpan.org>

--- lib/Spreadsheet/Reader/ExcelXML.pm
+++ lib/Spreadsheet/Reader/ExcelXML.pm
@@ -1,5 +1,5 @@
 package Spreadsheet::Reader::ExcelXML;
-use version 0.77; our $VERSION = version->declare('v0.1_23');
+use version 0.77; our $VERSION = version->declare('v0.1_25');
 ###LogSD	warn "You uncovered internal logging statements for Spreadsheet::Reader::ExcelXML-$VERSION";
 
 use 5.010;
