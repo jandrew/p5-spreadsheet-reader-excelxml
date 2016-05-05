bb6e5b4d0c5e08426e949fd2d851d9d06bee9a6f
Jed Lund <jandrew@cpan.org>
2016-05-05 08:03:58 -0700

    v0.1_7 + Another attempt to fix IO::Handle seek for debian
    Signed-off-by: Jed Lund <jandrew@cpan.org>

--- lib/Spreadsheet/Reader/ExcelXML.pm
+++ lib/Spreadsheet/Reader/ExcelXML.pm
@@ -1,5 +1,5 @@
 package Spreadsheet::Reader::ExcelXML;
-use version 0.77; our $VERSION = version->declare('v0.1_5');
+use version 0.77; our $VERSION = version->declare('v0.1_7');
 ###LogSD	warn "You uncovered internal logging statements for Spreadsheet::Reader::ExcelXML-$VERSION";
 
 use 5.010;
